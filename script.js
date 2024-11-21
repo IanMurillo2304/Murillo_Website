08c8f3752f0504a68e67274264c32cd596f3c960
IanMurillo2304 <ianbenedictmurillo2304@gmail.com>
2024-11-21 23:13:55 +0800

    Finished

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    const hamburger = document.querySelector('.hamburger-icon');
+    navLinks.classList.toggle('open');
+    hamburger.classList.toggle('open');
+  }
+  
